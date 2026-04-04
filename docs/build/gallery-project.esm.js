e99a0565819d8f53c4c0ce5eace26228129d4845
Anni Maasik <amaasik@gmail.com>
2026-04-04 10:23:31 +0300

    Add readme file

--- docs/build/gallery-project.esm.js
+++ docs/build/gallery-project.esm.js
@@ -1,1 +1,50 @@
-import{p as a,b as e}from"./p-DUnvxRq4.js";export{s as setNonce}from"./p-DUnvxRq4.js";import{g as r}from"./p-DQuL1Twl.js";(()=>{const s=import.meta.url,e={};return""!==s&&(e.resourcesUrl=new URL(".",s).href),a(e)})().then((async s=>(await r(),e([["p-a9b88c02",[[513,"image-gallery",{currentIndex:[32],isModalOpen:[32],isMobile:[32]}]]]],s))));
+import { B as BUILD, c as consoleDevInfo, H, w as win, N as NAMESPACE, p as promiseResolve, g as globalScripts, b as bootstrapLazy } from './index-CTWNaD4H.js';
+export { s as setNonce } from './index-CTWNaD4H.js';
+
+/*
+ Stencil Client Patch Browser v4.43.3 | MIT Licensed | https://stenciljs.com
+ */
+
+var patchBrowser = () => {
+  if (BUILD.isDev && !BUILD.isTesting) {
+    consoleDevInfo("Running in development mode.");
+  }
+  if (BUILD.cloneNodeFix) {
+    patchCloneNodeFix(H.prototype);
+  }
+  const scriptElm = BUILD.scriptDataOpts ? win.document && Array.from(win.document.querySelectorAll("script")).find(
+    (s) => new RegExp(`/${NAMESPACE}(\\.esm)?\\.js($|\\?|#)`).test(s.src) || s.getAttribute("data-stencil-namespace") === NAMESPACE
+  ) : null;
+  const importMeta = import.meta.url;
+  const opts = BUILD.scriptDataOpts ? (scriptElm || {})["data-opts"] || {} : {};
+  if (importMeta !== "") {
+    opts.resourcesUrl = new URL(".", importMeta).href;
+  }
+  return promiseResolve(opts);
+};
+var patchCloneNodeFix = (HTMLElementPrototype) => {
+  const nativeCloneNodeFn = HTMLElementPrototype.cloneNode;
+  HTMLElementPrototype.cloneNode = function(deep) {
+    if (this.nodeName === "TEMPLATE") {
+      return nativeCloneNodeFn.call(this, deep);
+    }
+    const clonedNode = nativeCloneNodeFn.call(this, false);
+    const srcChildNodes = this.childNodes;
+    if (deep) {
+      for (let i = 0; i < srcChildNodes.length; i++) {
+        if (srcChildNodes[i].nodeType !== 2) {
+          clonedNode.appendChild(srcChildNodes[i].cloneNode(true));
+        }
+      }
+    }
+    return clonedNode;
+  };
+};
+
+patchBrowser().then(async (options) => {
+  await globalScripts();
+  return bootstrapLazy([["image-gallery",[[513,"image-gallery",{"currentIndex":[32],"isModalOpen":[32],"isMobile":[32]}]]]], options);
+});
+//# sourceMappingURL=gallery-project.esm.js.map
+
+//# sourceMappingURL=gallery-project.esm.js.map
